e24c893cb9da4d7561a792e5127a3b182a1393d8
Praisg <gavipraise@gmail.com>
2023-05-16 13:28:05 +0000

    python

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''return all mul of tw2'''
+
 def divisible_by_2(my_list=[]):
    mul = []
-    for i in range(len(my_list)):
+for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             mul.append(True)
         else:
             mul.append(False)
-
-    return (mul)
+        return(mul)
 
--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
-'''switch value'''
+c = a, a = b, b = a
+print(f"a="+a+"b="+b)
