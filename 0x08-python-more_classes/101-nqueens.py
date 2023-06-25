85cfc21fdc5c2ff0271def2e713a6dd6b2563a4f
Praisg <gavipraise@gmail.com>
2023-06-25 16:09:38 +0000

    software

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Task 101"""
+
+import sys
+
+def is_safe(board, row, col, n):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
+
+    # Check if there is a queen in the top left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
+
+    # Check if there is a queen in the top right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True
+
+def solve_n_queens(board, row, n):
+    if row == n:
+        print_board(board)
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 'Q'
+            solve_n_queens(board, row+1, n)
+            board[row][col] = '.'
+
+    return False
+
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    solve_n_queens(board, 0, n)
+
+if __name__ == '__main__':
+    main()
+"""End Task"""
