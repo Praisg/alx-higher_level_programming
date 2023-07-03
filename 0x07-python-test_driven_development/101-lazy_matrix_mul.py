73930c685fab6410e0b063376d0d24feb8e18ad4
Praisg <gavipraise@gmail.com>
2023-07-03 20:59:29 +0000

    adv

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines  matrix malt function using NumPy."""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a : The first matrix.
+        m_b : The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
+
