a72110f5df29ca82a3784f8ffd9285b48265d3a4
Praisg <120684474+Praisg@users.noreply.github.com>
2023-11-04 21:59:16 +0200

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# send a request to an URL with curl, and displays size of the body of the response
-curl -s "$1" | wc -c
--- 0x10-python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends a request to URL, and displays only status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
